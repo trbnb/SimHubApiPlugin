9b21d730e9a71a5142d851ae2244b7c4e85e9b8c
Thorben Buchta <thorbenbuchta@gmail.com>
2022-10-23 13:22:42 +0200

    add gamedata definition

---
+++ SimHubApiPlugin/Models/GameData.cs
@@ -0,0 +1,91 @@
+namespace SimHubApiPlugin
+{
+    public class GameData
+    {
+        public double Speed { get; set; }
+        public double BrakeBias { get; set; }
+        public string Gear { get; set; }
+
+        public float Rpm { get; set; }
+        public float MaxRpm { get; set; }
+        public float Redline { get; set; }
+
+        public long CurrentLapTime { get; set; }
+        public long LastLapTime { get; set; }
+        public long BestLapTime { get; set; }
+        public float DeltaInSeconds { get; set; }
+        public bool IsLapValid { get; set; }
+
+        public int CurrentLap { get; set; }
+        public int? TotalLaps { get; set; }
+        public long SessionTimeLeft { get; set; }
+
+        public bool IsInPitlane { get; set; }
+        public bool IsPitLimiterOn { get; set; }
+
+        public int TC1Level { get; set; }
+        public bool TC1Active { get; set; }
+        public int TC2 { get; set; }
+        public bool TC2Active { get; set; }
+        public int ABS { get; set; }
+        public bool ABSActive { get; set; }
+
+        public int? EngineMap { get; set; }
+        public bool? IsLightOn { get; set; }
+        
+        public string TyreCompound { get; set; }
+        public WheelInfos WheelInfos { get; set; }
+
+        public float FuelRemaining { get; set; }
+        public float FuelPerLap { get; set; }
+        public float FuelRemainingLaps { get; set; }
+
+        public FlagState FlagState { get; set; }
+
+        public int Position { get; set; }
+        public int TotalPositions { get; set; }
+
+        public DrsState DrsState { get; set; }
+
+        public bool ErsAvailable { get; set; }
+        public float ErsBattery { get; set; }
+        public double ErsAllowance { get; set; }
+    }
+}
+
+public class WheelInfo {
+    public string Temperature { get; set; }
+    public string Health { get; set; }
+    public string Pressure { get; set; }
+}
+
+public class WheelInfos {
+    public WheelInfo FrontLeft { get; set; }
+    public WheelInfo FrontRight { get; set; }
+    public WheelInfo RearLeft { get; set; }
+    public WheelInfo RearRight { get; set; }
+}
+
+public enum DrsState {
+    None, Available, Enabled
+}
+
+public class FlagState {
+    public bool Black { get; set; }
+    public bool Orange { get; set; }
+    public bool Blue { get; set; }
+    public bool Checkered { get; set; }
+    public bool Green { get; set; }
+    public string Name { get; set; }
+    public bool White { get; set; }
+    public bool Yellow { get; set; }
+}
+
+public class ErsState {
+    public float BatteryCharge { get; set; }
+    public float RemainingLapAllowance { get; set; }
+    public int MguHMode { get; set; }
+    public int MguKMode { get; set; }
+    public int RecoveryLevel { get; set; }
+    public int EngineBrake { get; set; }
+}
---
+++ SimHubApiPlugin/TODO.md
@@ -0,0 +1,37 @@
+- gear
+- speed
+- brake bias
+
+- rpm indicator
+
+- Current laptime
+- Last lap
+- Best lap
+- delta
+- is lap valid
+
+- current lap/laps to complete session
+- time left
+
+- In pitlane/pit limiter
+
+- TC, TCC, ABS (IsActive)
+- engine map
+- lights on
+- ignition/engine state
+
+- tyre compound
+- tyre info (pressure, temperature, wear)
+
+- fuel per lap
+- fuel remaining
+- fuel remaining laps
+
+- flags
+
+- Position/Last position
+
+- drs (incl beep)
+- ers (charge, allowance, mguh mode, mguk mode, recovery level)
+
+- phase?
